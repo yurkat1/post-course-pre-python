18c04fe978cf7b95a2785e07919f79484fa28d0e
Kateryna <katerynatrybusyan@gmail.com>
2024-12-14 21:11:03 +0100

    initial commit

---
+++ main.py
@@ -0,0 +1,84 @@
+# print("Hello world")
+#
+# print("Hi, my name is Kate")
+# print()
+# print("What is your name?")
+# print()
+#
+# # escape последовательности
+# print("Hello\nworld")
+# print("apple\t\tjuice")
+# print("strawbary jam \"Rose\"")
+#
+# print("\n")
+# # sep  end
+# print("Hi", "Hello", "Guten Tag")
+# print("Hi", "Hello", "Guten Tag", sep=",", end="- ")
+# print("Bonjur")
+#
+# apple_pay = "iphone"
+# print(apple_pay)
+#
+# al_b = 1
+# print(al_b)
+#
+# # input
+# print("\n" *2)
+#
+#
+# name = input("Enter your name: ")
+# # age = input("Enter your age:")
+# age = int(input("Enter your age: "))
+#
+# # print("Name: " + name , "Age: " + age)
+# print(f"Name : {name} Age: {age}")
+#
+# print()
+#
+# number = int(input("Enter 3-digit number: "))
+# n1 = number // 100
+# n2 = number // 10 % 10
+# n3 = number % 10
+#
+# result = n1 + n2 + n3
+# print(f"n1: {n1} \nn2: {n2} \nn3: {n3}")
+# print(f"Result: {result}")
+
+
+
+# HOMEWORK!!!
+# 1 Користувач вводить три цифри з клавіатури. Необхідно знайти суму чисел, добуток чисел.
+# number = int(input("Print 3 digits:"))
+# n1 = number // 100
+# n2 = number // 10 % 10
+# n3 = number % 10
+# result = n1 + n2 + n3
+# print(f"n1= {n1} \nn2= {n2} \nn3= {n3}")
+# print(f"Result: {result}")
+
+# 2. Напишіть програму, яка обчислює площу ромба. Користувач із клавіатури вводить довжину двох його діагоналей.
+# Площа ромба дорівнює половині добутку його діагоналей: S = (AC · BD)/2.
+
+# diagonal1= int(input("Enter diagonal1:"))
+# diagonal2= int(input("Enter diagonal2:"))
+# s_rhombus = (diagonal1) * (diagonal2) / 2
+# print(f"Area of rhombus= {s_rhombus}")
+
+# 3. Користувач вводить з клавіатури число, що складається із чотирьох цифр. Потрібно знайти добуток цифр.
+# Наприклад, якщо з клавіатури введено 1324 тоді результат буде - 1*3*2*4 = 24.
+
+# number = int(input("Enter 4-digit number: "))
+# n1 = number // 1000
+# n2 = number // 100 % 10
+# n3 = number // 10 % 10
+# n4 = number % 10
+# print(f"n1= {n1}, n2= {n2}, n3= {n3}, n4= {n4}")
+# result= n1 * n2 * n3 * n4
+# print(f"Result: {result}")
+# ,,,
+
+
+
+
+
+
