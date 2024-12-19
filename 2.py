6f6ed9006e59b67d2d8029d18aa47024485ffacd
Kateryna <katerynatrybusyan@gmail.com>
2024-12-19 19:39:10 +0100

    commit hw1

--- 2.py
+++ 2.py
@@ -4,24 +4,26 @@ n3 = int(input(" Enter 3st number: "))
 
 # max of 3
 if n1 > n2 > n3 :
-    print("n1")
+    print(n1)
 elif n2 > n1 >n3 :
-    print("n2")
+    print(n2)
 elif n3> n2 > n1 :
-    print("n3")
+    print(n3)
 else :
     print("incorrect numbers")
 
     # min of 3
 if n1 < n2 < n3 :
-    print("n1")
+    print(n1)
 elif n2 < n1 < n3 :
-    print("n2")
+    print(n2)
 elif n3 < n2 < n1 :
-    print("n3")
+    print(n3)
 else :
     print("incorrect numbers")
 
 # среднеарифметическое
-averege = (n1 + n2 +n3) / 3
-print("averege")
+averege = ( n1 + n2 +n3 ) / 3
+print(f"averege = {averege}")
+
+
