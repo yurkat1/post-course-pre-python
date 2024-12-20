40ca96e9cc82efc89975fb3c7e6f227a9647c05e
Kateryna <katerynatrybusyan@gmail.com>
2024-12-20 17:08:24 +0100

    commit hw2

--- 2.py
+++ 2.py
@@ -1,29 +1,45 @@
-n1 = int(input(" Enter 1st number: "))
-n2 = int(input(" Enter 2st number: "))
-n3 = int(input(" Enter 3st number: "))
+# n1 = int(input(" Enter 1st number: "))
+# n2 = int(input(" Enter 2st number: "))
+# n3 = int(input(" Enter 3st number: "))
+#
+# # max of 3
+# if n1 > n2 > n3 :
+#     print(n1)
+# elif n2 > n1 >n3 :
+#     print(n2)
+# elif n3> n2 > n1 :
+#     print(n3)
+# else :
+#     print("incorrect numbers")
+#
+#     # min of 3
+# if n1 < n2 < n3 :
+#     print(n1)
+# elif n2 < n1 < n3 :
+#     print(n2)
+# elif n3 < n2 < n1 :
+#     print(n3)
+# else :
+#     print("incorrect numbers")
+#
+# # среднеарифметическое
+# averege = ( n1 + n2 +n3 ) / 3
+# print(f"averege = {averege}")
 
-# max of 3
-if n1 > n2 > n3 :
-    print(n1)
-elif n2 > n1 >n3 :
-    print(n2)
-elif n3> n2 > n1 :
-    print(n3)
-else :
-    print("incorrect numbers")
 
-    # min of 3
-if n1 < n2 < n3 :
-    print(n1)
-elif n2 < n1 < n3 :
-    print(n2)
-elif n3 < n2 < n1 :
-    print(n3)
-else :
-    print("incorrect numbers")
-
-# среднеарифметическое
-averege = ( n1 + n2 +n3 ) / 3
-print(f"averege = {averege}")
-
-
+# 2. Користувач вводить з клавіатури кількість метрів.
+# Залежно від вибору користувача програма переводить метри милі, дюйми або ярди.
+#
+# meteres =  int(input("Enter number of meteres: "))
+# measurements = int(input("Enter type of measurements( miles= 1, inches= 2, yards= 3): "))
+# if measurements == 1 :
+#     miles = meteres * 0.00062137
+#     print(f"miles= {miles}")
+# elif measurements == 2 :
+#     inches = meteres * 39.370
+#     print(f"inches= {inches}")
+# elif measurements == 3 :
+#     yards = meteres * 1.0936
+#     print(f"yards= {yards}")
+# else :
+#     print("Wrong measurements! Enter 1, 2 or 3")
